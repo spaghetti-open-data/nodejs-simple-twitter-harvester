2655a8d09fb54d6ed7ad085e111c91e26a6348ac
Alessio Biancalana <dottorblaster@gmail.com>
2014-04-09 11:21:26 +0200

    Adding a comma to make things right

--- config_twitter.js
+++ config_twitter.js
@@ -4,7 +4,7 @@ var twitter_conf = {
   twit_consumer_key: 'YOUR_TWITTER_APP_CONSUMER_KEY',
   twit_consumer_secret: 'YOUR_TWITTER_APP_CONSUMER_SECRET',
   twit_access_token: 'YOUR_TWITTER_APP_ACCESS_TOKEN',
-  twit_token_secret: 'YOUR_TWITTER_APP_TOKEN_SECRET'
+  twit_token_secret: 'YOUR_TWITTER_APP_TOKEN_SECRET',
 
   // twitter seach
   twitter_harvest_search: '#HASHTAG_TO_SEARCH', // hashtags can be combined (example #apple OR #pear)
